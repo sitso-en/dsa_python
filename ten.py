127fbb1e374b433959b5938080257a55326820f6
Enoch Sitsofe <sitso.nkrumah@gmail.com>
2025-09-22 11:37:16 +0000

    Update ten.py

--- ten.py
+++ ten.py
@@ -10,7 +10,7 @@ async def pull_user_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     async with aiohttp.ClientSession() as request: #create a session called request to allow us to make HTTP requests
         async with request.get(url) as result: #retrieve the posts into result and clean request to ensure proper request connection
-            data = await result.json() #steadily convert response into Json format
+            data = await result.json() #steadily convert response into JSON format
             
             for i in range(5):# just print first 5 titles
                 print(data[i]["title"])
