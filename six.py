faea6c752818816e79055ecb0bef4bf3016e374a
Enoch Sitsofe <sitso.nkrumah@gmail.com>
2026-02-20 22:37:10 +0000

    Add function to find the second largest number
    Implement logic to find the second largest number in a list.

--- six.py
+++ six.py
@@ -2,7 +2,6 @@
 Write a function second_largest(lst) that finds the second largest number in a list without 
 using sorted()."""
 
-#i am genuinely not able to come up with the correct logic for the second largest yet so i provided for the largest
 def second_largest(lst):
     largest = 0
     for num in lst:
