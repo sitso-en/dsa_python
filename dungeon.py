53aca62dd3a208209fc6318d77a2c0008957a073
Enoch Sitsofe <sitso.nkrumah@gmail.com>
2026-02-24 16:50:05 +0000

    Change player name prompt to be more aggressive

--- dungeon.py
+++ dungeon.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-player_name = input("What is your name? ")
+player_name = input("What is your name punk bitch? ")
 health: int = 100
 gold:int = 50
 inventory: list = ["sword"]
